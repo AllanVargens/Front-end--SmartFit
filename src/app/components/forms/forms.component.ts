1e8c227e81c1f79856479add781a3e7090af4c60
AllanVargens <allanvargens@gmail.com>
2024-06-20 18:28:32 -0300

    feat: add formControl

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss',
 })
 export class FormsComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
   results = [];
+  formGroup!: FormGroup;
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.formGroup.value);
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
 }

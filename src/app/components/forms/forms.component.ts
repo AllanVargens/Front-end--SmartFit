08ae846ae6c4e8574fa4568570a7bd8ecc6f6fe6
AllanVargens <allanvargens@gmail.com>
2024-06-20 23:21:35 -0300

    feat: add filter to open units

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Location } from '../../types/location.interface';
 
 @Component({
   selector: 'app-forms',
@@ -16,19 +17,30 @@ export class FormsComponent {
     private unitsService: GetUnitsService
   ) {}
 
-  results = [];
+  results: Location[] = [];
+  filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
   ngOnInit(): void {
-    this.unitsService.getAllUnits().subscribe((data) => console.log(data));
+    this.unitsService.getAllUnits().subscribe((data) => {
+      this.results = data.locations;
+      this.filteredResults = data.locations;
+    });
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false,
+      showClosed: true,
     });
   }
 
   onSubmit(): void {
     console.log(this.formGroup.value);
+    if (!this.formGroup.value.showClosed) {
+      this.filteredResults = this.results.filter(
+        (locations) => locations.opened === true
+      );
+    } else {
+      this.filteredResults = this.results;
+    }
   }
 
   onClean(): void {

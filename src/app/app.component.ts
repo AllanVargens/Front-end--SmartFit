9ef5edb670a75a14834b0c372176a6e14db9bd7b
AllanVargens <allanvargens@gmail.com>
2024-06-29 01:14:28 -0300

    feat: add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { CommonModule } from '@angular/common';
 import { Location } from './types/location.interface';
 import { GetUnitsService } from './services/get-units.service';
 import { LegendComponent } from './components/legend/legend.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +19,8 @@ import { LegendComponent } from './components/legend/legend.component';
     FormsComponent,
     CardsListComponent,
     CommonModule,
-    LegendComponent
+    LegendComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',

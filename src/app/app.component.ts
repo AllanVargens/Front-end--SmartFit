b06933a94aafff6f47a7e55140ced1095397ea72
AllanVargens <allanvargens@gmail.com>
2024-06-29 01:07:07 -0300

    feat: add legend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { CardsListComponent } from './components/cards-list/cards-list.component
 import { CommonModule } from '@angular/common';
 import { Location } from './types/location.interface';
 import { GetUnitsService } from './services/get-units.service';
+import { LegendComponent } from './components/legend/legend.component';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,7 @@ import { GetUnitsService } from './services/get-units.service';
     FormsComponent,
     CardsListComponent,
     CommonModule,
+    LegendComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
